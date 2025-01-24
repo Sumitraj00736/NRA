45e3296df76cf5beb6cf775ee1de1193b8099fc4
Sumitraj00736 <sumitraj00736@gmail.com>
2025-01-24 13:49:11 +0530

    Resolved merge conflicts

--- src/components/specific/HeroSlider.jsx
+++ src/components/specific/HeroSlider.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import "../../assert/HeroSlider.css";
 
 function HeroSlider() {
   const slides = [
@@ -16,8 +17,8 @@ function HeroSlider() {
     speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    centerMode: true,
-    centerPadding: "0",
+    centerMode: true, 
+    centerPadding: "160px", 
     focusOnSelect: true,
     autoplay: true,
     autoplaySpeed: 3000,
@@ -28,7 +29,7 @@ function HeroSlider() {
         settings: {
           slidesToShow: 1,
           centerMode: true,
-          centerPadding: "0",
+          centerPadding: "30%", 
         },
       },
     ],
@@ -38,16 +39,16 @@ function HeroSlider() {
     <div className="slider-container bg-customBlue h-[600px] flex justify-center items-center">
       <Slider {...settings} className="w-full max-w-[1200px]">
         {slides.map((slide) => (
-          <div key={slide.id} className="flex justify-center items-center">
-            <div className="relative flex flex-col justify-center items-center text-white">
-              <div className="bg-[#0b1f4f] w-[250px] h-[250px] flex justify-center items-center rounded-full mb-8">
+          <div key={slide.id} className="p-20 flex justify-center items-center">
+            <div className="bg-blue-900  w-[750px] h-[350px] relative rounded-2xl flex flex-col justify-center items-center text-white">
+              <div className=" flex justify-center items-center rounded mb-8">
                 <img
                   src={slide.icon}
                   alt={slide.title}
                   className="h-[150px] w-[150px] object-contain"
                 />
               </div>
-              <h2 className="text-2xl font-bold text-center mt-4">{slide.title}</h2>
+              <h2 className="text-2xl font-bold text-center mt-2">{slide.title}</h2>
             </div>
           </div>
         ))}
