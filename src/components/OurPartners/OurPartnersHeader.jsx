990490030c8825c1dfc4056920fcea5453c61aac
Tibrey <tibrashrestha8072@gmail.com>
2025-02-11 16:18:52 +0545

    feat:Update on Our partners

--- src/components/OurPartners/OurPartnersHeader.jsx
+++ src/components/OurPartners/OurPartnersHeader.jsx
@@ -38,11 +38,28 @@ const items = [
 function OurPartnersHeader() {
     const navigate = useNavigate(); // Initialize useNavigate
 
+    const navigateToMemberList = () => {
+        navigate('/member-list', { replace: true });
+    }
+
+    const handleScrollToTop = () => {
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth",
+        });
+      };
+
+
     return (
         <div className='mx-auto w-full max-w-7xl font-poppins px-2 mb-4 mt-20'>
-            <span className='inline-flex items-center gap-2 font-poppins border-2 px-4 py-2 rounded-xl mb-8 border-customOrange bg-customOrange bg-opacity-10'>
+            <span className='inline-flex items-center gap-2 font-poppins border-2 px-4 py-2 rounded-xl mb-8 border-customOrange bg-customOrange bg-opacity-10 cursor-pointer'
+            onClick={() => {
+                navigateToMemberList();
+                handleScrollToTop();
+            }}
+            >
                 <PiArrowUpRightBold className='text-customOrange' size={22} />
-                <h1 className='text-lg font-medium'>Our Partners</h1>
+                <h1 className='text-lg font-medium'>Our Members</h1>
             </span>
             
         </div>
